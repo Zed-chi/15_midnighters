3835dc0ca288ef1104f9db2dab72b9d5518c0170
AlexE <zed-chi@mail.ru>
2018-08-23 19:41:16 +0300

    quotes fix

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -4,23 +4,23 @@ import json
 from pytz import timezone
 
 
-def get_attempts_page(link, page):
+def fetch_attempts_json(link, page):
     response = requests.get(link, params={"page":page})
     return json.loads(response.text)
 
 
 def load_attempts(link):
     page = 1
-    attempts_page = get_attempts_page(link, page)
+    attempts_page = fetch_attempts_json(link, page)
     pages = attempts_page["number_of_pages"]
     att_list = attempts_page["records"]
     if pages > 1:
         for page in range(2, pages+1):
-            att_list += get_attempts_page(link, page)["records"]
+            att_list += fetch_attempts_json(link, page)["records"]
     return att_list
 
 
-def is_midnight(stamp, time_zone):
+def is_midnighter(stamp, time_zone):
     attempt_time = datetime.fromtimestamp(
         stamp,
         tz=timezone(time_zone),
@@ -33,12 +33,13 @@ def is_midnight(stamp, time_zone):
 def get_midnighters_names(attempts):
     midnighters = set()
     for attempt in attempts:
-        if is_midnight(attempt["timestamp"], attempt['timezone']):
+        if is_midnighter(attempt["timestamp"], attempt["timezone"]):
             midnighters.add(attempt["username"])
     return midnighters
 
 
 def print_midnighters(midnighters):
+    print("=== Midnighters ===")
     print("\n".join(midnighters))
 
 
@@ -49,5 +50,5 @@ def main():
     print_midnighters(midnighters_names)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
